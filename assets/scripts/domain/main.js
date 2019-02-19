2adc980f994178ad154c4be495b41d8bb59f3da9
Victor Germain <victor.germain@hetic.net>
2019-02-19 10:32:48 +0100

    Add sideBar

--- assets/scripts/domain/main.js
+++ assets/scripts/domain/main.js
@@ -1,1 +1,19 @@
-console.log('working yeah');
+console.log("working yeah");
+const menuBurger = document.querySelector(".burger");
+const sideMenu = document.querySelector(".side-menu");
+const anchors = document.querySelectorAll(".title-menu");
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener("click", () => {
+    toggleSlider();
+  });
+});
+
+menuBurger.addEventListener("click", () => {
+  toggleSlider();
+});
+
+const toggleSlider = () => {
+  menuBurger.classList.toggle("burgeractive");
+  sideMenu.classList.toggle("active");
+};
